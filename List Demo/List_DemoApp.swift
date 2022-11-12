a9babc08269562e100d95eba5864cbc656dc4882
chonhenry <chonhenry@gmail.com>
2022-11-12 15:42:22 -0500

    Initial Commit

---
+++ List Demo/List_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_DemoApp.swift
+//  List Demo
+//
+//  Created by Henry Chon on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct List_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
