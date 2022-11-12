9c76f3b2e3f9e8c93a28e9ca587ad204b9471291
chonhenry <chonhenry@gmail.com>
2022-11-12 17:49:43 -0500

    commit

--- List Demo/ContentView.swift
+++ List Demo/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var numbers = [Int]()
+    
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List(numbers, id: \.self) { n in
+                Text(String(n))
+            }
+            
+            HStack {
+                Spacer()
+                Button("Add until 7") {
+                    var randNum:Int
+                    repeat {
+                        randNum = Int.random(in: 0...10)
+                        numbers.append(randNum)
+                    } while randNum != 7
+                }
+                Spacer()
+                Button("Increase by 1") {
+                    if numbers.count > 0 {
+                        for i in 0...numbers.count-1 {
+                            numbers[i] += 1
+                        }
+                    }
+                }
+                Spacer()
+                Button("Clear") {
+                    numbers = []
+                }
+                Spacer()
+            }
+        }
     }
 }
 
